bdde03b74420a73ec81be283efc6501ad275e39e
Sid-Darade <128823616+Sid-Darade@users.noreply.github.com>
2023-09-03 21:49:31 +0530

    Delete Java directory

--- Java/WebAppDemo/target/WebAppDemo/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello World, I am Siddhesh Darade!!!</h2>
-</body>
-</html>
--- Java/booking-microservice-app/src/main/java/com/cdac/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cdac.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cdac.entity.Booking;
-
-public interface BookingRepository extends JpaRepository<Booking, Integer> {
-
-}
